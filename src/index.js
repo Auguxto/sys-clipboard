c01932cee9e796ef51f51b843ecacaad1b7bacae
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2023-08-18 20:31:58 -0300

    update: check system

--- src/index.js
+++ src/index.js
@@ -8,34 +8,30 @@ const node_child_process_1 = __importDefault(require("node:child_process"));
 const node_os_1 = __importDefault(require("node:os"));
 const platform = node_os_1.default.platform();
 function read() {
-    switch (platform) {
-        case "win32":
-            try {
-                return Buffer.from(node_child_process_1.default.execFileSync("src/exe/clipboard.exe", ["--read"])).toString("utf-8");
-            }
-            catch {
-                return "";
-            }
-        default:
+    if (platform === "win32") {
+        try {
+            return Buffer.from(node_child_process_1.default.execFileSync("src/exe/clipboard.exe", ["--read"])).toString("utf-8");
+        }
+        catch (err) {
+            console.log(err);
             return "";
+        }
     }
+    return "";
 }
 exports.read = read;
 function write(text) {
-    switch (platform) {
-        case "win32":
-            try {
-                Buffer.from(node_child_process_1.default.execFileSync("src/exe/clipboard.exe", [
-                    `--write=${text}`,
-                ])).toString("utf-8");
-                return true;
-            }
-            catch {
-                return false;
-            }
-        default:
+    if (platform === "win32") {
+        try {
+            Buffer.from(node_child_process_1.default.execFileSync("src/exe/clipboard.exe", [`--write=${text}`])).toString("utf-8");
+            return true;
+        }
+        catch (err) {
+            console.log(err);
             return false;
+        }
     }
+    return false;
 }
 exports.write = write;
 const clipboard = {
--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,34 @@ import os from "node:os";
 const platform = os.platform();
 
 export function read(): string {
-  switch (platform) {
-    case "win32":
-      try {
-        return Buffer.from(
-          child_process.execFileSync("src/exe/clipboard.exe", ["--read"])
-        ).toString("utf-8");
-      } catch (err) {
-        console.log(err);
-        return "";
-      }
-
-    default:
+  if (platform === "win32") {
+    try {
+      return Buffer.from(
+        child_process.execFileSync("src/exe/clipboard.exe", ["--read"])
+      ).toString("utf-8");
+    } catch (err) {
+      console.log(err);
       return "";
+    }
   }
+
+  return "";
 }
 
 export function write(text: string): boolean {
-  switch (platform) {
-    case "win32":
-      try {
-        Buffer.from(
-          child_process.execFileSync("src/exe/clipboard.exe", [
-            `--write=${text}`,
-          ])
-        ).toString("utf-8");
+  if (platform === "win32") {
+    try {
+      Buffer.from(
+        child_process.execFileSync("src/exe/clipboard.exe", [`--write=${text}`])
+      ).toString("utf-8");
 
-        return true;
-      } catch (err) {
-        console.log(err);
-        return false;
-      }
-
-    default:
+      return true;
+    } catch (err) {
+      console.log(err);
       return false;
+    }
   }
+  return false;
 }
 
 const clipboard = {
