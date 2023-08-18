5884ddcae9feb43f37aa7666417d032f5bac77ca
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2023-08-18 13:17:54 -0300

    remove: lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare function read(): string;
-declare function write(text: string): string;
-declare const clipboard: {
-    read: typeof read;
-    write: typeof write;
-};
-export default clipboard;
